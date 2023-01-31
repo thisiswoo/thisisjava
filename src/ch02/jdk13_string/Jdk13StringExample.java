c1f94fba7d6c0b964720e4a27360bb05b0ba07d2
thisiswoo <thisiswoo0594@gmail.com>
2023-01-31 09:42:38 +0900

    edit jdk13 String

--- src/ch02/jdk13_string/Jdk13StringExample.java
+++ src/ch02/jdk13_string/Jdk13StringExample.java
@@ -3,7 +3,7 @@ package ch02.jdk13_string;
 public class Jdk13StringExample {
     public static void main(String[] args) {
 
-        // JDK 15+
+        // JDK 13+
         String str = """
                 AAA
                 BBB
@@ -14,5 +14,35 @@ public class Jdk13StringExample {
         // AAA
         // BBB
         // CCC
+        System.out.println(str.length());
+        System.out.println(str.replace("\n", "D"));
+
+        System.out.println();
+
+        String str2 = "" +
+                "{\n" +
+                "\t\"id\":\"winter\",\n" +
+                "\t\"name\":\"눈송이\",\n" +
+                "}";
+        String str3 = """
+                {
+                    "id":"winter",
+                    "name":"눈송이"
+                }
+                """;
+        String str4 = """
+                나는 자바를 \
+                학습합니다.
+                나는 자바 고수가 될 겁니다.
+                """;
+        System.out.println(str2);
+
+        System.out.println();
+
+        System.out.println(str3);
+
+        System.out.println();
+
+        System.out.println(str4);
     }
 }
