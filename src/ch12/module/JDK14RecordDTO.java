014e7e921dabd876a8c25ac500d874a2b7a02d1b
thisiswoo <thisiswoo0594@gmail.com>
2023-02-02 15:24:10 +0900

    add JDK14+ record example

--- src/ch12/module/JDK14RecordDTO.java
+++ src/ch12/module/JDK14RecordDTO.java
@@ -1,4 +1,7 @@
 package ch12.module;
 
 public record JDK14RecordDTO(String id, String name, int age) {
+    // JDK14+ record 메서드는 자동으로
+    // private final 필드와 생성자 및 Getter 메서드가 자동으로 추가 되고,
+    // hashCode(), equals(), toString() 메서드를 재정의한 코드도 자동으로 추가된다.
 }
--- src/ch12/module/JDK14RecordDTOExample.java
+++ src/ch12/module/JDK14RecordDTOExample.java
@@ -2,6 +2,10 @@ package ch12.module;
 
 public class JDK14RecordDTOExample {
     public static void main(String[] args) {
+        // JDK14+ record 메서드는 자동으로
+        // private final 필드와 생성자 및 Getter 메서드가 자동으로 추가 되고,
+        // hashCode(), equals(), toString() 메서드를 재정의한 코드도 자동으로 추가된다.
+
         JDK14RecordDTO r = new JDK14RecordDTO("winter", "눈송이", 25);
         System.out.println(r.id());
         System.out.println(r.name());
