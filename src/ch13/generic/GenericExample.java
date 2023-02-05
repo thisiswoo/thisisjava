207444c74e486ea9cc9ea3bbc4703a041e351bdd
thisiswoo <thisiswoo0594@gmail.com>
2023-02-05 10:41:11 +0900

    add bounded type parameter generic example

--- src/ch13/generic/GenericExample.java
+++ src/ch13/generic/GenericExample.java
@@ -77,6 +77,19 @@ public class GenericExample {
         String strValue = b7.getT();
         System.out.println(strValue);
 
+        System.out.println();
+
+        // 제한된 타입 파라미터 (Bounded Type Parameter) ------------
+        // 제네릭 메서드 호출
+        boolean result3 = compare(10, 20);  // T를 Integer 타입으로 대체
+        System.out.println(result3);
+
+        System.out.println();
+
+        // 제네릭 메서드 호출
+        boolean result4 = compare(4.5, 4.5);    // T를 Double 타입으로 대체
+        System.out.println(result4);
+
     }
 
     // Generic Method ---------------------------------
@@ -87,9 +100,14 @@ public class GenericExample {
         return box;
     }
 
-    // 제한된 타입 파라미터 ---------------------------------
+    // 제한된 타입 파라미터 (Bounded Type Parameter) --------
     // 제한된 타입 파라미터를 갖는 제네릭 메서드
-    /*public static <T extends Number> boolean compare(T t1, T t2) { // 타입 파라미터 T를 대체할 타입을 Number로 제한
-
-    }*/
+    public static <T extends Number> boolean compare(T t1, T t2) { // 타입 파라미터 T를 대체할 타입을 Number로 제한
+        // T의 타입을 출력
+        System.out.println("compare(" + t1.getClass().getSimpleName() + ", " + t2.getClass().getSimpleName() + ")");
+        // Number의 메서드 사용
+        double v1 = t1.doubleValue();   // Number의 doubleValue() 메서드 사용
+        double v2 = t2.doubleValue();   // Number의 doubleValue() 메서드 사용
+        return (v1 == v2);
+    }
 }
