70d04bd1b909b292a45234d43c9af72dc5a95414
thisiswoo <thisiswoo0594@gmail.com>
2023-02-11 21:56:44 +0900

    add thread sleep, sumThread Example

---
+++ src/ch14/multi_thread/JoinExample.java
@@ -0,0 +1,13 @@
+package ch14.multi_thread;
+
+public class JoinExample {
+    public static void main(String[] args) {
+        SumThread sumThread = new SumThread();
+        sumThread.start();
+        try {
+            sumThread.join();
+        } catch (InterruptedException e) {
+        }
+        System.out.println("1 ~ 100 합 : " + sumThread.getSum());
+    }
+}
