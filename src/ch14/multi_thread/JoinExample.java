6fc96b5b5d9d3f8e27680e176c4ee58443780da8
thisiswoo <thisiswoo0594@gmail.com>
2023-02-15 07:53:13 +0900

    add thread sleep, sumThread Example

--- src/ch14/multi_thread/JoinExample.java
+++ src/ch14/multi_thread/JoinExample.java
@@ -4,10 +4,12 @@ public class JoinExample {
     public static void main(String[] args) {
         SumThread sumThread = new SumThread();
         sumThread.start();
+        // 1. try catch 를 사용해서 출력해보기 - 5050
         try {
             sumThread.join();
         } catch (InterruptedException e) {
         }
+        // 2. try catch 주석하여 출력해보기 - 0
         System.out.println("1 ~ 100 합 : " + sumThread.getSum());
     }
 }
